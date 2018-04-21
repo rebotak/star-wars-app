1f0661a3d840f532345c9eca05e840b83f49cbc8
Rebotak <rebotak@gmail.com>
2018-04-21 17:35:13 +0700

    setup router, redux, store, middleware.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './assets/scss/main.css';
+import './assets/scss/font-awesome.min.css';
+import Homepage from './containers/Homepage';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
+
+const App = () => (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <Route path="/" component={Homepage}/>
+        </Router>
       </div>
-    );
-  }
-}
-
+    )
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Provider } from 'react-redux';
+import Store, { history } from './store';
+import { ConnectedRouter } from 'react-router-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+render(
+	<Provider store={Store}>
+		<ConnectedRouter history={history}>
+			<App />
+		</ConnectedRouter>
+	</Provider>
+	, document.getElementById('root'));
 registerServiceWorker();
